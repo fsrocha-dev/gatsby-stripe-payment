f9a57f995c1d6c5daab47820d7d6e9c1bf60cd70
fsrocha <contato.frankrocha@gmail.com>
2020-02-14 13:22:18 -0300

    add effect header scroll menu and change state

--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,52 @@ import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <header className="HeaderGroup">
-      <Link to="/">
-        <img
-          src={require('../images/logo-designcode.svg')}
-          width="30"
-          alt="Logo"
-        />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshop">Workshop</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
-    </header>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className={this.state.hasScrolled ? 'Header HeaderScroll' : 'Header'}
+      >
+        <header className="HeaderGroup">
+          <Link to="/">
+            <img
+              src={require('../images/logo-designcode.svg')}
+              width="30"
+              alt="Logo"
+            />
+          </Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshop">Workshop</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </header>
+      </div>
+    )
+  }
+}
 
 export default Header
